53ba9b82079e447c7331bd20d5ef04ad19793d07
leaviki90 <leaviki90@gmail.com>
2025-02-04 12:37:22 +0100

    Small update

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import tags from '../test-data/tags.json';
 
+
 // Before each test, set up the environment
 // This includes mocking API responses and navigating to the test page
 
@@ -21,5 +22,23 @@ test.beforeEach(async ({ page }) => {
 
 // Test case: Verify that the title in the navbar is correct
 test('has title', async ({ page }) => {
+    //intercepting
+    await page.route('*/**/api/articles*', async route => {
+        const response = await route.fetch()
+        const responseBody = await response.json()
+        responseBody.articles[0].title = "This is a MOCK test title"  //articles is the name of array from response
+        //title is object value
+        responseBody.articles[0].description = "This is a MOCK description"
+         //description is object value also
+
+       await route.fulfill({
+           body: JSON.stringify(responseBody)
+       })
+   })
+
+    await page.getByText('Global Feed').click()
     await expect(page.locator('.navbar-brand')).toHaveText('conduit');
-});
+    await expect(page.locator('app-article-list h1').first()).toContainText('This is a MOCK test title');
+    await expect(page.locator('app-article-list p').first()).toContainText('This is a MOCK description');
+   
+}); 
